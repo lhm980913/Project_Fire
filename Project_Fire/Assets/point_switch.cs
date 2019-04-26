9f3e566550598dbfefd945425191ef0b06dd4619
Zoryx <15801610913@163.com>
2019-04-27 00:19:08 +0800

    no message

---
+++ Project_Fire/Assets/point_switch.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class point_switch : MonoBehaviour
+{
+    public GameObject door;
+    Transform rape_manager;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rape_manager = GameObject.FindGameObjectWithTag("rape_manager").transform;
+        GameObject go = Instantiate(door, this.transform.position, Quaternion.identity) as GameObject;
+        go.transform.SetParent(rape_manager);
+        //chuancan(go, i, j);
+        go.layer = LayerMask.NameToLayer("Rape_Point");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
