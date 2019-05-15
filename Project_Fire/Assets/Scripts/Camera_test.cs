9b9f57325f3bf086b6a52afd4b0482b8febb99a9
DESKTOP-L4LLM8G\60294 <602941661@qq.com>
2019-05-16 02:43:04 +0800

    no message

--- Project_Fire/Assets/Scripts/Camera_test.cs
+++ Project_Fire/Assets/Scripts/Camera_test.cs
@@ -1,42 +1,52 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Cinemachine;
 
 public class Camera_test : UnityEngine.MonoBehaviour
 {
-    Vector3 a = Vector3.zero;
-    public float ttt;
-    public float z;
+    public enum CurrentFOV
+    {
+        Normal,
+        Zoom
+    }
 
-    public float z_max;
+    Vector3 a = Vector3.zero;
+    public float NormalFOV;
+    public float ZoomFOV;
+    public CinemachineVirtualCamera Vcam;
+    public Transform cameraTarget;
+
+    private CurrentFOV fovStage;
+    CinemachineBrain brain;
+    
 
     bool aix;
     // Start is called before the first frame update
     void Start()
     {
-        z = -z_max;
+        brain = GetComponent<CinemachineBrain>();
         aix = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        //this.transform.position = Vector3.SmoothDamp(
-        //    this.transform.position,
-        //    new Vector3(testplayer.Instance.playergameobj.transform.position.x, testplayer.Instance.playergameobj.transform.position.y, this.transform.position.z), ref a, ttt);
-        this.transform.position = new Vector3(testplayer.Instance.transform.position.x, testplayer.Instance.transform.position.y, z);
+        //this.transform.position = new Vector3(testplayer.Instance.transform.position.x, testplayer.Instance.transform.position.y, z);
 
         if(Input.GetKeyDown(KeyCode.O)||Player_Controller_System.Instance.Xbox_Y==-1)
         {
             if(aix)
             {
-                if (z == -z_max)
+                if (fovStage == CurrentFOV.Normal)
                 {
-                    z = -5;
+                    Vcam.m_Lens.FieldOfView = ZoomFOV;
+                    fovStage = CurrentFOV.Zoom;
                 }
                 else
                 {
-                    z = -z_max;
+                    Vcam.m_Lens.FieldOfView = NormalFOV;
+                    fovStage = CurrentFOV.Normal;
                 }
             }
             aix = false;
@@ -46,6 +56,13 @@ public class Camera_test : UnityEngine.MonoBehaviour
             aix = true;
         }
     
-
+        if(Player_Controller_System.Instance.Vertical_Left < 0.0 && testplayer.Instance.grounded)
+        {
+            cameraTarget.localPosition = new Vector3(0, -2.5f, 0);
+        }
+        else
+        {
+            cameraTarget.localPosition = Vector3.zero;
+        }
     }
 }
