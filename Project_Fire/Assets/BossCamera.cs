8848a18f42dac5c08113ff62b6d2c12658d2ad13
DESKTOP-L4LLM8G\60294 <602941661@qq.com>
2019-06-14 21:31:32 +0800

    no message

---
+++ Project_Fire/Assets/BossCamera.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class BossCamera : MonoBehaviour
+{
+    public CinemachineVirtualCamera MainCamera;
+    public CinemachineVirtualCamera Boss;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            MainCamera.enabled = false;
+            Boss.enabled = true;
+        }
+    }
+}
